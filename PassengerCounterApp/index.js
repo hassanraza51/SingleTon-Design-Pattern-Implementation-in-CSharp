13e0d9b171fc7b390013725557fd98a7a657305e
Hassan Raza <razahassan616@gmail.com>
2022-04-30 01:30:14 +0500

    deleted

--- PassengerCounterApp/index.js
+++
@@ -1,9 +0,0 @@
-//document.getElementById("count-el").innerText = 6;
-var counter = document.getElementById("count-el");
-var button = document.getElementById("increment-btn");
-let count = 0;
-button.onclick = function () {
-  count++;
-  console.log(count);
-  counter.innerHTML = count;
-};
