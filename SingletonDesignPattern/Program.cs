d023096d6e903c66e830461933a54e9976908203
Hassan Raza <razahassan616@gmail.com>
2022-05-16 02:56:32 +0500

    Add files via upload

---
+++ SingletonDesignPattern/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace SingletonDesignPattern
+{
+    public class Program
+    {
+        static TableServers host1List = TableServers.GetTableServers();
+        static TableServers host2List = TableServers.GetTableServers();
+        static TableServers host3List = TableServers.GetTableServers();
+
+        public static void Main(string[] args)
+        {
+            //TableServers servers = TableServers.GetTableServers();
+            for (int i = 0; i < 6; i++)
+            {
+                Host1GetNextServer();
+                Host2GetNextServer();
+                Host3GetNextServer();
+            }
+            Console.ReadLine();
+        }
+        private static void Host1GetNextServer()
+        {
+            Console.WriteLine("Next Server is: " + host1List.getNextServer());
+        }
+        private static void Host2GetNextServer()
+        {
+            Console.WriteLine("Next Server is: " + host2List.getNextServer());
+        }
+        private static void Host3GetNextServer()
+        {
+            Console.WriteLine("Next Server is: " + host3List.getNextServer());
+        }
+    }
+}
---
+++ SingletonDesignPattern/TableServers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace SingletonDesignPattern
+{
+	public class TableServers
+	{
+        private static readonly TableServers _instance = new TableServers();
+        private List<string> servers = new List<string>();
+        private int nextServer = 0;
+
+        private TableServers()
+        {
+            servers.Add("Hassan");
+            servers.Add("Raza");
+            servers.Add("Ali");
+            servers.Add("Ahmad");
+        }
+        public static TableServers GetTableServers()
+        {
+            return _instance;
+        }
+        public string getNextServer()
+        {
+            string output = servers[nextServer];
+            nextServer += 1;
+            if (nextServer >= servers.Count)
+                nextServer = 0;
+            return output;
+        }
+    }
+}
+
